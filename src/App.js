f03280b84ae748d148879ee8dd8fbc39258c650b
1999-Nidhi <nidhi.goel@ablore.com>
2022-12-17 15:13:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        h1 className = 'error' > Error < /h1> <
+        h1 className = { styles.success } > Success < /h1> <
+        Stylesheet primary = { true } > < /Stylesheet> <
+        Inline > < /Inline> <
+        /div>
+    );
 }
 
 export default App;
